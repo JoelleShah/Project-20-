61c0dde9bb36e816db4f653be77d1e4630d17901
JoelleShah <121088109+JoelleShah@users.noreply.github.com>
2023-03-15 19:34:00 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var s1
+
+function setup() {
+  createCanvas(400,400);
+ s1 = new Student ("John",12,6)
+}
+
+function draw() 
+{
+  background(30);
+  s1.show()
+}
---
+++ student.js
@@ -0,0 +1,15 @@
+
+class Student
+{
+  constructor (name,age,grade){
+    this.name = name 
+    this.age = age
+    this.grade = grade
+  }
+ show(){
+    console.log(this.name,this.age,this.grade)
+ }
+}
+
+
+
